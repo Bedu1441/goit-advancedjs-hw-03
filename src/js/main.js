a79d821febebae37cda30a4b9e7ac4082c62fe4e
Bedu1441 <b.stoietskyi@gmail.com>
2026-05-02 19:25:17 -0400

    Complete homework 3 image search

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
 import '../css/styles.css';
 
 import { getImagesByQuery } from './pixabay-api.js';
@@ -8,4 +11,47 @@ import {
   hideLoader,
 } from './render-functions.js';
 
-console.log('Homework 3 setup is ready');
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const query = form.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search query',
+      position: 'topRight',
+    });
+
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (!data.hits.length) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(() => {
+      iziToast.error({
+        message: 'Something went wrong. Please try again later.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      hideLoader();
+      form.reset();
+    });
+});
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '55691735-3ea2edc83d49145f84f9678df';
+const BASE_URL = 'https://pixabay.com/api/';
+
 export function getImagesByQuery(query) {
-  return Promise.resolve({
-    hits: [],
-    totalHits: 0,
-  });
+  const params = {
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  };
+
+  return axios.get(BASE_URL, { params }).then(response => response.data);
 }
